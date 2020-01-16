a2180db192f5cfe0245666d24f2a9d7934be913d
Greg Willard (r3pwn) <wickett06@gmail.com>
2020-01-16 11:44:35 -0500

    [api] add endpoint to get the API version

--- server.py
+++ server.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
+import json
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/hello")
-def hello():
-    return "Hello World!"
-
+@app.route("/version")
+def appinfo():
+    with open('appinfo.json') as json_file:
+        data = json.load(json_file)
+        for p in data['appinfo']:
+            return p['version']
 
 if __name__ == '__main__':
     app.run(debug=True)
