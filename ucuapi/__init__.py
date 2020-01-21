112ed02c5f432ecdc7c77484d23683a78b9ab3f5
Greg Willard (r3pwn) <wickett06@gmail.com>
2020-01-21 08:57:20 -0500

    [main] split main into its own module for easier maintenance

--- ucuapi/__init__.py
+++ ucuapi/__init__.py
@@ -1,18 +1,13 @@
-import json
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/version")
-def appinfo():
-    with open('appinfo.json') as json_file:
-        data = json.load(json_file)
-        for p in data['appinfo']:
-            return p['version']
-
 # load modules here
+from ucuapi.main import main
 from ucuapi.protocols.btle.module import btle
 from ucuapi.protocols.zigbee.module import zigbee
 
+app.register_blueprint(main, url_prefix='/')
 app.register_blueprint(btle, url_prefix='/btle')
 app.register_blueprint(zigbee, url_prefix='/zigbee')
+
